9b76745c7c640d2669636d7baac1a36bc45ccab1
lfesp <l.f.esparraguera@outlook.com>
2022-05-17 22:37:58 -0400

    updated job history

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -10,11 +10,11 @@ const Banner = () => {
                 <div>
                 <h1 className="subheading">Hi! I'm Liam Esparraguera.</h1>
                 <div className="main-text thin">
-                  <p>I'm a student at Princeton University pursuing a B.S.E. in Computer Science, as well as certificates (minors) in Music Composition, Statistics and Machine Learning, and Technology and Society.</p>
+                  <p>I'm a student at Princeton University pursuing a B.S.E. in Computer Science, as well as certificates (minors) in Music Performance, Statistics and Machine Learning, and Technology and Society.</p>
                   <p>Some of my key interests are web development, music composition, and product design, and my passion is combining aesthetics and functionality to create technologies that bring people together in meaningful ways.</p>
                 </div>
                 </div>
-                <div className="main-image"><StaticImage src="../images/website_headshot.jpg" alt="Headshot of Liam Esparraguera." height={400}/></div>
+                <div className="main-image"><StaticImage src="../images/website_headshot.jpg" alt="Headshot of Liam Esparraguera." height={400} /></div>
             </div>
         </Section>
     );
